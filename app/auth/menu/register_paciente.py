6b1a1242bff3b23f1f536614a834bea835f12b5e
pedroX4vier <pedroh.moura2024@gmail.com>
2025-10-10 14:45:20 -0300

    feat: Foi adicionado a função de salvar a acão quando resgistrar um novo paciente.

--- app/auth/menu/register_paciente.py
+++ app/auth/menu/register_paciente.py
@@ -1,11 +1,24 @@
 import json
 import os
+from app.utils.historico_utils import Historico
 
+
+# Pegando o nome do profissional
+def getNameProfissional():
+  caminho = 'app/data/users.json'
+  
+  if os.path.exists(caminho):
+    with open(caminho, 'r') as file:
+      users = json.load(file)
+      for user in users:
+        if user.get("isLogged") == 1:
+          return user
+  return None
 
 def cadastrar_paciente():
     print("\n--- CADASTRO DE PACIENTES ---")
     nome = input("Nome: ")
-    cpf = input("CPF: ")
+    cpf = input("CPF: ") 
     email = input("E-mail ")
     # pacientes = {"nome": nome, "cpf": cpf, "E-mail": email}
 
@@ -38,6 +51,21 @@ def cadastrar_paciente():
     
     with open(caminho, "w", encoding="utf-8") as file:
       json.dump(users, file, indent=2, ensure_ascii=False)
+      
+    
+    # Salvando a ação no histórico
+    user = getNameProfissional()
+    historico_paciente = "app/data/historico_paciente/historico_paciente.json"
+    historico = Historico(arquivo=historico_paciente)
+    historico.registrarHistoricoPaciente(
+      acao="Cadastro de pasciente",         # pega a ação
+      paciente=novo_paciente["nome"],       # Pega o nome do paciente
+      email=novo_paciente["email"],         # Pega o email do profissonal
+      profissional=user['nome'],            #  Pega o nome do profissional
+      especializacao_prof=user['especializacao']
+    )
+      
+      
 
 
 
