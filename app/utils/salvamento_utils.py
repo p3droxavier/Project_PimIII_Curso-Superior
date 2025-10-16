25a85937a68e82c5dddb3ceea15acb0ef359eed3
pedroX4vier <pedroh.moura2024@gmail.com>
2025-10-16 18:16:06 -0300

    feat: Foi criado o arquivo para fazer os salvamentos do sistema

---
+++ app/utils/salvamento_utils.py
@@ -0,0 +1,46 @@
+import json, os
+from datetime import datetime
+
+class Salvamento:
+  def __init__(self, arquivo = None):
+    self.arquivo = arquivo
+    
+    # Verifica se o arquivo existe,  e se o seu tamanho é igual a 0
+    if not os.path.exists(self.arquivo) or os.path.getsize(arquivo) == 0:
+      
+      with open(self.arquivo, "w", encoding="utf-8") as file:
+        json.dump([], file, indent=2, ensure_ascii=False)
+      
+
+  # Carrega o arquivo para salvar
+  def _carregar(self):
+    with open(self.arquivo, "r", encoding="utf-8") as file:
+      conteudo = file.read().strip()
+      
+      if not conteudo:
+        return []
+      dados = json.loads(conteudo)
+      if not isinstance(dados, list):
+        return []
+      return dados
+    
+  
+  # Salva no no arquivo
+  def _salvar(self, dados):
+    with open(self.arquivo, "w", encoding="utf-8") as file:
+      json.dump(dados, file, indent=2, ensure_ascii=False)
+      
+      
+      
+  def registrar_usuario(self, dados):
+    usuario = {
+      "Dados do Usuário": dados,
+      "Data do salvamento":datetime.now().strftime("%d/%m/%y %H:%M:%S")
+    }
+    
+    dados = self._carregar()
+    dados.append(usuario)
+    self._salvar(dados)
+    
+        
+    
