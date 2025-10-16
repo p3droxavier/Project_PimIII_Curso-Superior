44829537be1ec5a327aea607e0a21e36f29efe62
pedroX4vier <pedroh.moura2024@gmail.com>
2025-10-16 18:17:34 -0300

    feat: Foi alterado o modo de salvamento do cadastro

--- app/auth/register.py
+++ app/auth/register.py
@@ -2,6 +2,7 @@ import bcrypt
 import json
 import os
 from app.utils.historico_utils import Historico
+from app.utils.salvamento_utils import Salvamento
 
 # Função para cadastrar um novo funcionario
 def registerProfissional():
@@ -38,38 +39,18 @@ def registerProfissional():
         "isLogged":0 # Verifica se alguem  esta logado
       }
       
-      # Caminho do arquivo onde sera salvo o usuário
-      caminho = 'app/data/users.json'
       
-      # Verifica a exixtencia e o tamanho do arquivo
-      if not os.path.exists(caminho) or os.path.getsize(caminho) == 0:
-        users = []
-        
-      # Abrindo arquivo para leitura 
-      else: 
-        with open(caminho, "r", encoding="utf-8") as file:
-          conteudo = file.read().strip() # le o conteudo do arquivo como string, remove espaços em branco 
-          
-          # Depois do strip o conteudo for uma string vazia "", não ha dados validos
-          if not conteudo:
-            users = []
-            
-          else: 
-            users = json.loads(conteudo)
-            # Garante que users seja uma lista 
-            if not isinstance(users, list):
-              users = []
-              
-      users.append(new_user)
-      
-      with open(caminho, "w", encoding="utf-8") as file:
-        json.dump(users, file, indent=2, ensure_ascii=False)
+      # Faz o salvamento do usuário no arquivo .json
+      caminho_salvamento = 'app/data/users.json'
+      salvar = Salvamento(arquivo=caminho_salvamento)
+      salvar.registrar(dados=new_user)
+
         
         
       # Salvando a ação no histórico
       historico_user = "app/data/historico_user/historico_user.json"
       historico = Historico(arquivo=historico_user)
-      historico.registrar(acao="Cadastro de usuário", usuario=new_user["nome"])
+      historico.registrar_usuario(acao="Cadastro de usuário", usuario=new_user["nome"])
         
       
       # Chama a função de gerar a UI da dashboard
